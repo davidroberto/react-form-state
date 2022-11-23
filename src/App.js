ebcc2162c234be14737ea379010bfdc3a30adeb6
David Robert <contact@david-robert.fr>
2022-11-23 13:57:42 +0100

    state with arrays

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [formValues, setFormValues] = useState(["", "", null]);
+  
+  const handleForm = (event) => {
+    event.preventDefault();
+    const lastName = event.target.lastName.value;
+    const firstName = event.target.firstName.value;
+    const age = event.target.age.value;
+
+    setFormValues([lastName, firstName, age])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleForm}>
+          <label>LastName
+            <input type="text" name="lastName"></input>
+          </label>
+
+          <label>FirstName
+            <input type="text" name="firstName"></input>
+          </label>
+
+          <label>Age
+            <input type="number" name="age"></input>
+          </label>
+
+          <input type="submit"></input>
+      </form>
+
+      <p>{formValues[0]}</p>
+      <p>{formValues[1]}</p>
+      <p>{formValues[2]}</p>
+   </>
   );
 }
 
